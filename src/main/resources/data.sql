ff12c4867b09b6ac845cae767bcad12b3d13d54f
jkaszczynski <j.kaszczynski@gmail.com>
2020-02-09 19:51:52 +0100

    Prepopulate table

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO Quotes (id, quote, created_date, modified_date ,displayed_date )
+VALUES
+(1, 'test1', {ts '2020-02-09 00:00:00.00'}, null, null),
+(2, 'test2', {ts '2020-02-09 00:00:00.00'}, null, null),
+(3, 'test3', {ts '2020-02-09 00:00:00.00'}, null, null),
+(4, 'test4', {ts '2020-02-09 00:00:00.00'}, null, null),
+(5, 'test5', {ts '2020-02-09 00:00:00.00'}, null, null);
